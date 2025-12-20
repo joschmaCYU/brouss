c9bca372636b181d387f0c8e3650736ddf29f22b
joschmaCYU <joschka.mayer@etu.cyu.fr>
2025-12-20 19:11:37 +0100

    you can now add integers together !

---
+++ build/out.asm
@@ -0,0 +1,18 @@
+global _start
+_start:
+    mov rax, 8
+    push rax
+    mov rax, 1
+    push rax
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+    push QWORD [rsp + 0]
+
+    mov rax, 60
+    pop rdi
+    syscall
+    mov rax, 60
+    mov rdi, 0
+    syscall
