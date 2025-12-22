0124456233d9a4f2bf8555b0b72bba23078a20ef
joschmaCYU <joschka.mayer@etu.cyu.fr>
2025-12-22 15:09:57 +0100

    added multiplication division and parentheses

--- build/out.asm
+++ build/out.asm
@@ -1,18 +1,18 @@
 global _start
 _start:
-    mov rax, 8
-    push rax
     mov rax, 2
     push rax
-    mov rax, 2
-    push rax
-    pop rax
-    pop rbx
-    mul rbx
+    mov rax, 1
     push rax
     pop rax
     pop rbx
     add rax, rbx
+    push rax
+    mov rax, 6
+    push rax
+    pop rax
+    pop rbx
+    div rbx
     push rax
     push QWORD [rsp + 0]
 
